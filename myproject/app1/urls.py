d28c250c4b41a3b2d62587cc954d7aaffbe549a6
abhishek-pratap <iapsingh@outlook.com>
2025-11-13 12:02:49 +0530

    change it

--- myproject/app1/urls.py
+++ myproject/app1/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.home, name='home'),  # Route for the home view in app1
+    path ('', views.home, name='home '),  # Route for the home view in app1
     
 ]
